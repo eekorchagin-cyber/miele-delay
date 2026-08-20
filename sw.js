3dd9343cebc1bf284357b4755f474a8a000c3b93
eekorchagin-cyber <eekorchagin@gmail.com>
2026-08-20 22:50:42 +0700

    Add files via upload
    Upload Miele Delay 1.0

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="miele-delay-1.0.0";
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
